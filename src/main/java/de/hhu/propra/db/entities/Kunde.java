758030aeb68317599035ba05183384915996aba8
Katharina Cembik <colognejune@googlemail.com>
2018-11-15 15:07:46 +0100

    Code freigeschaltet

--- src/main/java/de/hhu/propra/db/entities/Kunde.java
+++ src/main/java/de/hhu/propra/db/entities/Kunde.java
@@ -2,10 +2,8 @@ package de.hhu.propra.db.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data // Lombok - Generiert Getter, Setter, toString, ...
 @Entity
