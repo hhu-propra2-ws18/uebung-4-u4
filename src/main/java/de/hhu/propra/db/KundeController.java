c47c12c88ddc370e4328021ce6dbbc34d0893e7f
Katharina Cembik <colognejune@googlemail.com>
2018-11-18 13:12:32 +0100

    Thymeleaf, Abfragen

--- src/main/java/de/hhu/propra/db/KundeController.java
+++ src/main/java/de/hhu/propra/db/KundeController.java
@@ -1,6 +1,7 @@
 package de.hhu.propra.db;
 
 import de.hhu.propra.db.data.KundeRepository;
+import de.hhu.propra.db.entities.Kunde;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,8 @@ public class KundeController{
 
     @GetMapping("/")
     public String index(Model model) {
+        Iterable<Kunde> kunden2 = kunden.findAll();
+        model.addAttribute("kunden", kunden2);
         return "index";
     }
 }
