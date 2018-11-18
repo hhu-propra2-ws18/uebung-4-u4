e1d834ef92c52687d90f167d9c3f3409833d7e6b
Katharina Cembik <colognejune@googlemail.com>
2018-11-18 01:49:12 +0100

    Auto Eigenschaften und Annotation, Kunde Datenmodell

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,12 @@
 package de.hhu.propra.db.entities;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Auto{
 
+    private int id;
+    private String marke;
+    private String farbe;
+    private double hubraum;
 }
