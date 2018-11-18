5cc3376039960ede1b9c6801b307a8d925901844
Katharina Cembik <colognejune@googlemail.com>
2018-11-18 01:58:38 +0100

    Auto Key, Databaseinitializer freigeschaltet

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,10 +1,18 @@
 package de.hhu.propra.db.entities;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
 @Entity
 public class Auto{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String marke;
     private String farbe;
